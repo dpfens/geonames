4d220aed18b6172a1f1e1d1ecf42c6d15f5f5d6a
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2022-02-27 13:43:11 -0500

    simplifies queries

--- spatial.sql
+++ spatial.sql
@@ -1,27 +1,23 @@
+USE geonames;
+
 CREATE TABLE geoNameSpatial (
   `geonameid` INT UNSIGNED PRIMARY KEY COMMENT "Integer id of record in geonames database",
   `coordinates` POINT NOT NULL COMMENT "Latitude-Longitude coordinates of record"
 ) ROW_FORMAT=COMPRESSED;
 
+CREATE SPATIAL INDEX sx_geonames_coords ON geoNameSpatial(coordinates);
+
 INSERT INTO geoNameSpatial (geoNameId, coordinates)
     SELECT geoName.geoNameId, ST_GeomFromText(CONCAT('POINT (', geoName.latitude, ' ', geoName.longitude, ')'), 4326)
-    FROM geoName
-      LEFT JOIN geoNameSpatial
-        ON geoName.geoNameId=geoNameSpatial.geoNameId
-    WHERE geoNameSpatial.geoNameId is NULL;
-
-CREATE SPATIAL INDEX sx_geonames_coords ON geoNameSpatial(coordinates);
+    FROM geoName;
 
 CREATE TABLE `shapesSpatial` (
   `geonameid` INT UNSIGNED PRIMARY KEY COMMENT "Integer id of record in geonames database",
   `geom` GEOMETRY NOT NULL
 ) ROW_FORMAT=COMPRESSED;
 
-INSERT INTO shapesSpatial (geoNameId, geom)
-    SELECT geoName.geoNameId, ST_GeomFromGeoJSON(geoJson, 1)
-    FROM shapes
-      LEFT JOIN shapesSpatial
-        ON shapes.geoNameId=shapesSpatial.geoNameId
-    WHERE shapesSpatial.geoNameId is NULL;
-
 CREATE SPATIAL INDEX sx_shapes_geom ON shapesSpatial(geom);
+
+INSERT INTO shapesSpatial (geoNameId, geom)
+    SELECT shapes.geoNameId, ST_GeomFromGeoJSON(geoJson, 1)
+    FROM shapes;
