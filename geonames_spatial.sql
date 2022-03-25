7288e4636e087cdc1139973867c4c9924e2c5baf
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2022-03-25 10:37:14 -0400

    adds default SRID to spatial columns to ensure MySQL 8 is able to use spatial indexes

--- geonames_spatial.sql
+++ geonames_spatial.sql
@@ -2,14 +2,14 @@ USE geonames;
 
 CREATE TABLE geoNameSpatial (
   `geonameid` INT UNSIGNED PRIMARY KEY COMMENT "Integer id of record in geonames database",
-  `coordinates` POINT NOT NULL COMMENT "Latitude-Longitude coordinates of record"
+  `coordinates` POINT NOT NULL SRID 4326 COMMENT "Latitude-Longitude coordinates of record"
 ) ROW_FORMAT=COMPRESSED;
 
 CREATE SPATIAL INDEX sx_geonames_coords ON geoNameSpatial(coordinates);
 
 CREATE TABLE `shapesSpatial` (
   `geonameid` INT UNSIGNED PRIMARY KEY COMMENT "Integer id of record in geonames database",
-  `geom` GEOMETRY NOT NULL
+  `geom` GEOMETRY NOT NULL SRID 4326
 ) ROW_FORMAT=COMPRESSED;
 
 CREATE SPATIAL INDEX sx_shapes_geom ON shapesSpatial(geom);
